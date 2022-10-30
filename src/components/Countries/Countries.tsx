b1d46f537b9c9907f941774c6237913df2fd5edf
AnthonyBrancato <abrancato.contact@gmail.com>
2022-10-30 21:11:13 +0100

    fix(): forgot to move the query manager to the view

--- src/components/Countries/Countries.tsx
+++ src/components/Countries/Countries.tsx
@@ -1,6 +1,5 @@
 import AppBar from 'components/AppBar';
 import CountriesList from 'components/CountriesList';
-import CountriesListQueryManager from 'components/CountriesList/CountriesListQueryManager';
 import CountriesSearch from 'components/CountriesSearch';
 import * as React from 'react';
 
@@ -8,12 +7,10 @@ function Countries() {
   return (
     <React.Fragment>
       <AppBar />
-      <CountriesListQueryManager>
-        {/* <CountriesSearchFilterManager> */}
-        <CountriesSearch />
-        <CountriesList />
-        {/* </CountriesSearchFilterManager> */}
-      </CountriesListQueryManager>
+      {/* <CountriesSearchFilterManager> */}
+      <CountriesSearch />
+      <CountriesList />
+      {/* </CountriesSearchFilterManager> */}
     </React.Fragment>
   );
 }
--- src/pages/CountriesView.tsx
+++ src/pages/CountriesView.tsx
@@ -1,10 +1,13 @@
 import { Countries } from 'components';
+import CountriesListQueryManager from 'components/CountriesList/CountriesListQueryManager';
 import * as React from 'react';
 
 export function CountriesView() {
   return (
     <React.Fragment>
-      <Countries />
+      <CountriesListQueryManager>
+        <Countries />
+      </CountriesListQueryManager>
     </React.Fragment>
   );
 }
